1906b6688d08c7ca2e3457b4716122720f7b46fb
Mr.Radion <mr.rodion_oa@mail.ru>
2020-11-12 14:01:09 +0300

    Creating a header and initial architecture

---
+++ src/components/appJobSeeker/taskCreationForms/taskCreation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function taskCreation() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default taskCreation
---
+++ src/components/appPerformer/myAssignments/myAssignments.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function myAssignments() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default myAssignments
