80ab6e905a0bf39cd35435b17c2c8e99e98bdbe2
Mr.Radion <mr.rodion_oa@mail.ru>
2020-11-12 17:13:01 +0300

    Adding files to the page folder

---
+++ src/pages/Routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { landingJobSeeker, landingPerformer } from './';
+
+function Routes() {
+  return (
+      <>
+      </>
+  );
+}
