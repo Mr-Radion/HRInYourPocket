0162bd646a52091ac308d6af0dbdd6c0925334f2
Mr.Radion <mr.rodion_oa@mail.ru>
2020-11-15 18:02:14 +0300

    Adding initial markup to the header and navigation

--- src/components/landingJobSeeker/LandingHeader.tsx
+++ src/components/landingJobSeeker/LandingHeader.tsx
@@ -1,22 +1,50 @@
 import React from 'react';
-import logo from '../../static/landingJobSeeker/header/logo.svg';
+import LandingNav from './LandingNav';
 
 function LandingHeader() {
+  const toForm = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="landing-header">
+      <div className="landingHeader-shape"> 
+        <span className="landingHeader-logo" />
+        <LandingNav />
+      </div>
+      <div className="landingHeader-content">
+        <div className="landingHeader-content-txt">
+          <h1>
+            Устройся на работу <br />
+            мечты
+          </h1>
+          <span>
+            HR в кармане - это веб-платформа, которая позволяет соискателю создавать задания на
+            поиск работы с оплатой за приглашения. Вам останется лишь ходить на интервью.
+          </span>
+          <fieldset className="landingContainer__form">
+            <div>
+              <input
+                className="landingItem__form"
+                name="vocation"
+                type="vocation"
+                placeholder="Желаемая специальность"
+                required
+              />
+              <input
+                className="landingItem__form"
+                name="link-to-resume"
+                type="link-to-resume"
+                placeholder="Ссылка на резюме"
+                required
+              />
+            </div>
+          </fieldset>
+          <button className="btn btn-danger header-btn" onClick={toForm}>
+            Создать задание
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/landingJobSeeker/LandingNav.tsx
+++ src/components/landingJobSeeker/LandingNav.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function LandingNav() {
+  const toForm = (e: any) => {
+    e.preventDefault();
+    window.scrollTo({
+      left: 0,
+      top: 6000,
+      behavior: 'smooth',
+    });
+  };
   return (
     <div>
-      
+      <button className="btn btn-danger header-btn" onClick={toForm}>
+        Оставить заявку
+      </button>
+      <button className="btn btn-danger header-btn">
+        Войти
+      </button>
     </div>
-  )
+  );
 }
 
-export default LandingNav
+export default LandingNav;
