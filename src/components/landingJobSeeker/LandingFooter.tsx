afd98f8997da5d68e82dab0fb363b3a75bf60d0c
Mr.Radion <mr.rodion_oa@mail.ru>
2020-11-17 01:51:29 +0300

    Creating the initial footer markup

--- src/components/landingJobSeeker/LandingFooter.tsx
+++ src/components/landingJobSeeker/LandingFooter.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function LandingFooter() {
   return (
-    <div>
-      
-    </div>
+    <footer className="landing-footer">
+      <div className="landingFooterLeft">
+        <div className="landingFooter-navWrapper">
+          <ul className="landingFooter-navList">
+            <li className="landingFooter-navList__item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="landingFooterRight">
+        <span className="logo" />
+      </div>
+    </footer>
   )
 }
 
