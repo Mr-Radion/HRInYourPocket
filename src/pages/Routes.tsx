45c032d3dfef0e6623cfe6e421d01ffac102a760
Mr.Radion <mr.rodion_oa@mail.ru>
2020-12-28 19:15:16 +0300

    Adding an auth clause when displaying routes

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { LandingJobSeeker, Home, PageNotFound, MyTasks, TaskCreation, RegistrationForm, Authorization } from './';
+import {
+  LandingJobSeeker,
+  Home,
+  PageNotFound,
+  MyTasks,
+  TaskCreation,
+  RegistrationForm,
+  Authorization,
+} from './';
 
 export default function Routes() {
+  const isAuth = useSelector(({ user }: any) => user.isAuth);
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={LandingJobSeeker} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/auth" component={Authorization} />
-        <Route exact path="/register" component={RegistrationForm} />
-        <Route exact path="/task-creation" component={TaskCreation} />
-        <Route exact path="/my-tasks" component={MyTasks} />
-        <Route exact path="/my-tasks/:id" />
+      {/* {isAuth && ()} */}
+      {!isAuth && (
+        <Switch>
+          <Route exact path="/" component={LandingJobSeeker} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/auth" component={Authorization} />
+          <Route exact path="/register" component={RegistrationForm} />
+          <Route exact path="/task-creation" component={TaskCreation} />
+          <Route exact path="/my-tasks" component={MyTasks} />
+          <Route exact path="/my-tasks/:id" />
 
-        {/* <Route exact path="/">
+          {/* <Route exact path="/">
           <Redirect to="/home" />
           <Route path="/home" component={Home} />
         </Route> */}
-        <Route path="*" component={PageNotFound} />
-      </Switch>
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      )}
     </>
   );
 }
